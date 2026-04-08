72e2c4b40210175369033762e0dfd48520353240
Tommaso Moretta <tmoretta2001@gmail.com>
2026-04-08 15:03:36 +0200

    Project initialized

---
+++ src/test/java/it/itconsulting/progettofinalebackendtommasogabriel/ProgettofinalebackendtommasogabrielApplicationTests.java
@@ -0,0 +1,13 @@
+package it.itconsulting.progettofinalebackendtommasogabriel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettofinalebackendtommasogabrielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
