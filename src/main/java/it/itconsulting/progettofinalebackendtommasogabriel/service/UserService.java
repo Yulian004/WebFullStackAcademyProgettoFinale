ba10c517ecd0967d4a23402732fa9312af0573c9
Tommaso Moretta <tmoretta2001@gmail.com>
2026-04-09 16:05:45 +0200

    Adjustments

--- src/main/java/it/itconsulting/progettofinalebackendtommasogabriel/service/UserService.java
+++ src/main/java/it/itconsulting/progettofinalebackendtommasogabriel/service/UserService.java
@@ -5,12 +5,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import it.itconsulting.progettofinalebackendtommasogabriel.dto.UserDto;
-import it.itconsulting.progettofinalebackendtommasogabriel.exceptions.user.UserAlreadyExistsException;
 import it.itconsulting.progettofinalebackendtommasogabriel.exceptions.user.UserNotFoundException;
 import it.itconsulting.progettofinalebackendtommasogabriel.exceptions.user.UserNotValidException;
 import it.itconsulting.progettofinalebackendtommasogabriel.model.User;
